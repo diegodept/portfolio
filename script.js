52caf520114d5cfbe91831f84840956e5ce4db4a
diegodept <dfsotorojas@gmail.com>
2025-07-30 23:11:43 +1200

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+const closeMenu = document.getElementById('close-menu');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+});
+
+
