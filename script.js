4c1fb81859d6ac5af3b8410bc8827ecaac5aed34
diegodept <dfsotorojas@gmail.com>
2025-07-31 14:19:55 +1200

    changes

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ closeMenu.addEventListener('click', () => {
   navLinks.classList.remove('active');
 });
 
+window.addEventListener("scroll", () => {
+  const overlay = document.querySelector(".parallax-overlay");
+  const scrollY = window.scrollY;
+  
+  // Move overlay slower for parallax effect
+  overlay.style.transform = `translate(-20%, ${scrollY * 0.2}px)`;
+});
+
+
 
